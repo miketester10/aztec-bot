3a75d4cb291b36d9490967c0c54fd6e0691443d6
miketester10 <cadenza91@gmail.com>
2025-07-09 05:24:38 +0200

    feat: add a simple method for antispam

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ config();
 
 const NODE_ENV: string = process.env.NODE_ENV!;
 const BOT_TOKEN: string = process.env.BOT_TOKEN!;
+const BLOCKED_USER_IDS = [5645790791, 72274003];
 const serverHandler: ServerHandler = ServerHandler.getInstance();
 const commandsHandler: CommandsHandler = CommandsHandler.getInstance();
 
@@ -25,33 +26,53 @@ const bot = new Bot(BOT_TOKEN).onStart(async (ctx) => {
 });
 // Handle Commands
 bot.command("network_health", async (ctx: MyMessageContext) => {
+  if (await isSpam(ctx)) return;
   await commandsHandler.newtworkHealth(ctx);
 });
 bot.command("active_nodes", async (ctx: MyMessageContext) => {
+  if (await isSpam(ctx)) return;
   await commandsHandler.activeNodesByCountry(ctx);
 });
 bot.command("node", async (ctx: MyMessageContext) => {
+  if (await isSpam(ctx)) return;
   await commandsHandler.handleNodeCommand(ctx);
-})
+});
 bot.command("validator", async (ctx: MyMessageContext) => {
+  if (await isSpam(ctx)) return;
   await commandsHandler.handleValidatorCommand(ctx);
 });
 bot.command("top10", async (ctx: MyMessageContext) => {
+  if (await isSpam(ctx)) return;
   await commandsHandler.handleTop10Command(ctx);
 });
 bot.command("epoch", async (ctx: MyMessageContext) => {
+  if (await isSpam(ctx)) return;
   await commandsHandler.handleEpochCommand(ctx);
 });
 bot.command("start", async (ctx: MyMessageContext) => {
+  if (await isSpam(ctx)) return;
   await commandsHandler.handleStartCommand(ctx);
 });
 bot.command("help", async (ctx: MyMessageContext) => {
+  if (await isSpam(ctx)) return;
   await commandsHandler.handleHelpCommand(ctx);
 });
 // Handle Callback
 bot.callbackQuery<RegExp>(/^.+$/, async (ctx: MyCallbackQueryContext) => {
+  if (await isSpam(ctx)) return;
   await commandsHandler.handleCallbackCommand(ctx);
 });
+
+const isSpam = async (
+  ctx: MyMessageContext | MyCallbackQueryContext
+): Promise<boolean> => {
+  const userId = ctx.from?.id;
+  if (userId && BLOCKED_USER_IDS.includes(userId)) {
+    await ctx.send("Too many requests, please DO NOT SPAM!");
+    return true;
+  }
+  return false;
+};
 
 const main = async () => {
   try {
